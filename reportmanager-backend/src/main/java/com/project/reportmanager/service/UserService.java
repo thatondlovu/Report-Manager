6fbfb8b1ee3043f5ed59491eedd0ed7e45c5d2f3
thatondlovu <thatondlovuCB@gmail.com>
2026-07-20 12:21:25 +0200

    added backend

---
+++ reportmanager-backend/src/main/java/com/project/reportmanager/service/UserService.java
@@ -0,0 +1,36 @@
+package com.project.reportmanager.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.project.reportmanager.model.User;
+import com.project.reportmanager.repository.UserRepository;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    // Register a new user (checking for duplicates first)
+    public User registerUser(User user) {
+        if (userRepository.findByUsername(user.getUsername()).isPresent()) {
+            throw new RuntimeException("Username is already taken!");
+        }
+        if (userRepository.findByStudentNumber(user.getStudentNumber()).isPresent()) {
+        throw new RuntimeException("An account with this Student Number already exists!");
+    }
+        return userRepository.save(user);
+    }
+
+    // Basic login verification (plain text for now to keep things simple for testing)
+    public User loginUser(String username, String password) {
+        Optional<User> userOpt = userRepository.findByUsername(username);
+        if (userOpt.isPresent() && userOpt.get().getPassword().equals(password)) {
+            return userOpt.get();
+        }
+        throw new RuntimeException("Invalid username or password!");
+    }
+}
---
+++ reportmanager-backend/src/test/java/com/project/reportmanager/ReportmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.reportmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportmanagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
