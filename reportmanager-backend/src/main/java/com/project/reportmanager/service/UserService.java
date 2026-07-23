b9ea1c5302788ec49f17ada3914e06a9f15793b9
thatondlovu <thatondlovuCB@gmail.com>
2026-07-23 14:33:42 +0200

    added encryption

--- reportmanager-backend/src/main/java/com/project/reportmanager/service/UserService.java
+++ reportmanager-backend/src/main/java/com/project/reportmanager/service/UserService.java
@@ -4,7 +4,6 @@ import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
