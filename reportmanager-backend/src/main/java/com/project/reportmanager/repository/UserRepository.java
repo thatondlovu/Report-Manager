923ce891d3b713b0e6e506c702cf836624f31899
thatondlovu <thatondlovuCB@gmail.com>
2026-07-28 13:02:01 +0200

    final backend changes

--- reportmanager-backend/src/main/java/com/project/reportmanager/repository/UserRepository.java
+++ reportmanager-backend/src/main/java/com/project/reportmanager/repository/UserRepository.java
@@ -10,8 +10,8 @@ import com.project.reportmanager.model.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     
-    Optional<User> findByUsername(String username);
-    Optional<User> findByStudentNumber(String studentNumber);
+    Optional<User> findByUsername(String username);    
+    Optional<User> findByStudentNumber(int studentNumber);
     boolean existsByUsername(String username);
-    boolean existsByStudentNumber(String studentNumber);
+    boolean existsByStudentNumber(int studentNumber);
 }
